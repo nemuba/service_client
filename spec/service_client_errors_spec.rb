a23ede6a922585c994a6085c27eb0441993d59c0
Alef Ojeda de Oliveira <alef.oliveira@dimensa.com.br>
2023-02-11 23:23:27 -0300

    added new tests

--- spec/service_client_errors_spec.rb
+++ spec/service_client_errors_spec.rb
@@ -1,9 +1,54 @@
 # frozen_string_literal: true
 
-RSpec.describe 'ServiceClient errors' do
-  ServiceClient::Errors::ERRORS.each do |code, error|
-    it "raise error with status code #{code}" do
-      expect { ServiceClient::Call.get("https://mock.codes/#{code}") }.to raise_error(error)
+RSpec.describe 'ServiceClient raise errors' do
+  let(:headers) { { content_type:  'application/json' } }
+  let(:body) { { request: { name: 'John Doe' } } }
+
+  describe 'GET' do
+    (400..511).to_a.each do |status|
+      next unless Rack::Utils::HTTP_STATUS_CODES.key?(status)
+
+      it "returns #{status} status" do
+        expect { 
+          ServiceClient::Call.get("https://mock-http-requests.herokuapp.com/#{status}")
+        }.to raise_error(ServiceClient::Errors::ERRORS[status.to_s.to_sym])
+      end
+    end
+  end
+
+  describe 'POST' do
+    (400..511).to_a.each do |status|
+      next unless Rack::Utils::HTTP_STATUS_CODES.key?(status)
+
+      it "returns #{status} status" do
+        expect { 
+          ServiceClient::Call.post("https://mock-http-requests.herokuapp.com/#{status}", headers: headers, body: body)
+        }.to raise_error(ServiceClient::Errors::ERRORS[status.to_s.to_sym])
+      end
+    end
+  end
+
+  describe 'PUT' do
+    (400..511).to_a.each do |status|
+      next unless Rack::Utils::HTTP_STATUS_CODES.key?(status)
+
+      it "returns #{status} status" do
+        expect { 
+          ServiceClient::Call.put("https://mock-http-requests.herokuapp.com/#{status}", headers: headers, body: body)
+        }.to raise_error(ServiceClient::Errors::ERRORS[status.to_s.to_sym])
+      end
+    end
+  end
+
+  describe 'DELETE' do
+    (400..511).to_a.each do |status|
+      next unless Rack::Utils::HTTP_STATUS_CODES.key?(status)
+
+      it "returns #{status} status" do
+        expect { 
+          ServiceClient::Call.delete("https://mock-http-requests.herokuapp.com/#{status}")
+        }.to raise_error(ServiceClient::Errors::ERRORS[status.to_s.to_sym])
+      end
     end
   end
 end
--- spec/service_client_spec.rb
+++ spec/service_client_spec.rb
@@ -1,32 +1,109 @@
+# frozen_string_literal: true
+
 RSpec.describe ServiceClient do
   it 'has a version number' do
     expect(ServiceClient::VERSION).not_to be nil
   end
 
   context 'Call' do
-    let(:call) { ServiceClient::Call.get('https://mock.codes/200') }
+    describe 'GET' do
+      let(:call) { ServiceClient::Call.get('https://mock-http-requests.herokuapp.com/200') }
 
-    it 'have an method with response class instance' do
-      expect(call).to be_a(ServiceClient::Response)
+      it 'have an method with response class instance' do
+        expect(call).to be_a(ServiceClient::Response)
+      end
+
+      it 'response have an method data' do
+        expect(call).to respond_to(:data)
+      end
+
+      it 'response have an method headers' do
+        expect(call).to respond_to(:headers)
+      end
+
+      it 'response have an method code' do
+        expect(call).to respond_to(:code)
+      end
+
+      it 'raise error when missing params' do
+        expect { ServiceClient::Call.get(nil) }.to raise_error(ServiceClient::ParamsRequired)
+      end
     end
 
-    it 'response have an method data' do
-      expect(call).to respond_to(:data)
+    describe 'POST' do
+      let(:body) { { request: { test: 'test' } } }
+      let(:headers) { { content_type: 'application/json' } }
+      let(:call) { ServiceClient::Call.post('https://mock-http-requests.herokuapp.com/200', headers: headers, body: body) }
+
+      it 'have an method with response class instance' do
+        expect(call).to be_a(ServiceClient::Response)
+      end
+
+      it 'response have an method data' do
+        expect(call).to respond_to(:data)
+      end
+
+      it 'response have an method headers' do
+        expect(call).to respond_to(:headers)
+      end
+
+      it 'response have an method code' do
+        expect(call).to respond_to(:code)
+      end
+
+      it 'raise error when missing params' do
+        expect { ServiceClient::Call.post(nil, body: nil) }.to raise_error(ServiceClient::ParamsRequired)
+      end
     end
 
-    it 'response have an method headers' do
-      expect(call).to respond_to(:headers)
-    end
+    describe 'PUT' do
+      let(:body) { { request: { test: 'test' } } }
+      let(:headers) { { content_type: 'application/json' } }
+      let(:call) { ServiceClient::Call.put('https://mock-http-requests.herokuapp.com/200', headers: headers, body: body) }
 
-    it 'response have an method code' do
-      expect(call).to respond_to(:code)
-    end
+      it 'have an method with response class instance' do
+        expect(call).to be_a(ServiceClient::Response)
+      end
 
-    it 'raise error when missing params' do
-      expect { ServiceClient::Call.get(nil) }.to raise_error(ServiceClient::ParamsRequired)
-      expect { ServiceClient::Call.post(nil, body: nil) }.to raise_error(ServiceClient::ParamsRequired)
-      expect { ServiceClient::Call.put(nil, body: nil) }.to raise_error(ServiceClient::ParamsRequired)
-      expect { ServiceClient::Call.delete(nil) }.to raise_error(ServiceClient::ParamsRequired)
+      it 'response have an method data' do
+        expect(call).to respond_to(:data)
+      end
+
+      it 'response have an method headers' do
+        expect(call).to respond_to(:headers)
+      end
+
+      it 'response have an method code' do
+        expect(call).to respond_to(:code)
+      end
+
+      it 'raise error when missing params' do
+        expect { ServiceClient::Call.put(nil, body: nil) }.to raise_error(ServiceClient::ParamsRequired)
+      end
+    end
+  
+    describe 'DELETE' do
+      let(:call) { ServiceClient::Call.delete('https://mock-http-requests.herokuapp.com/200') }
+
+      it 'have an method with response class instance' do
+        expect(call).to be_a(ServiceClient::Response)
+      end
+
+      it 'response have an method data' do
+        expect(call).to respond_to(:data)
+      end
+
+      it 'response have an method headers' do
+        expect(call).to respond_to(:headers)
+      end
+
+      it 'response have an method code' do
+        expect(call).to respond_to(:code)
+      end
+
+      it 'raise error when missing params' do
+        expect { ServiceClient::Call.delete(nil) }.to raise_error(ServiceClient::ParamsRequired)
+      end
     end
   end
 end
